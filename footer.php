ac6f31a7dcd8639d253c36a60771e6e7f5ff4446
dumitruonceanu <dumitru.onceanu@gmail.com>
2014-09-02 23:12:33 -0400

    added social icons and styled

--- footer.php
+++ footer.php
@@ -1,6 +1,25 @@
 <div class="footer">
 	<div class="innerWrapper">
-    <p>&copy; Dumitru Onceanu <?php echo date('Y'); ?></p>
+		<div class="socialWrapper">
+			<div class="icon twitter">
+				<a href="https://twitter.com/DumitruOnceanu" target="blank_"><img src="<?php bloginfo('template_directory'); ?>/icons/icon_20940_white.png"></a> <!-- Icon source: http://thenounproject.com/term/twitter/20940/ -->
+			</div>
+			<div class="icon facebook">
+				<a href="https://www.facebook.com/dumitru.onceanu" target="blank_"><img src="<?php bloginfo('template_directory'); ?>/icons/icon_20845_white.png"></a> <!-- Icon source: http://thenounproject.com/term/facebook/20845/ -->
+			</div>
+			<div class="icon mail">
+				<a href="mailto:dumitru@onceanu.com"><img src="<?php bloginfo('template_directory'); ?>/icons/icon_90_white.png"></a> <!-- Icon source: http://thenounproject.com/term/mail/90/ -->
+			</div>
+			<div class="icon linkedIn">
+				<a href="https://www.linkedin.com/in/dumitruonceanu" target="blank_"><img src="<?php bloginfo('template_directory'); ?>/icons/linkedIn.png"></a> <!-- Icon source: http://thenounproject.com/term/facebook/20845/ -->
+			</div>
+			<div class="icon gitHub">
+				<a href="https://github.com/dumitruonceanu" target="blank_"><img src="<?php bloginfo('template_directory'); ?>/icons/GitHub-Mark/PNG/GitHub-Mark-Light-32px.png"></a> <!-- Icon source:  -->
+			</div>
+
+			<div class="copy">&copy; Copyright <?php echo date('Y'); ?> Dumitru Onceanu</div>
+		</div>
+    
     <?php wp_footer(); ?>
   </div>
 </div>
